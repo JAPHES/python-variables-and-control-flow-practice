e090f30eeed0a53ce7142b2e2526a3454ae1699a
JAPHES <japhesmurithi@gmail.com>
2024-10-23 15:33:18 +0300

    first commit

---
+++ question1.py
@@ -0,0 +1,27 @@
+# Assign values to int input
+
+a =int(input('Enter number'))
+b =int(input('Enter number'))
+
+#perform arithmetic operations
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+
+
+division = a / b  # division will return a float result
+
+
+floor_division = a // b # floor_division return integer result
+
+# print the results
+
+print("addition:", addition)
+print("subtraction:", subtraction)
+print("multiprication:", multiplication)
+print("division:", division)
+
+
+
+
---
+++ question2.py
@@ -0,0 +1,8 @@
+# odd number is a number that when divided by 2 gives a whole number
+#  even number is a number that when divided by 2 gives a remainder
+
+x = int(input("Enter a number"))
+if x / 2 == 0:
+    print("This is an Even Number")
+else:
+    print("This is an Odd Number")
